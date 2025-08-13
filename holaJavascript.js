96c5cea98761744ab1170395309e87b4e6d7122c
avallea <avallealcega@gmail.com>
2025-08-13 13:38:34 +0200

    añadido archivo java
    introduccion a el funcionamiento de java en un archivo .js

---
+++ holaJavascript.js
@@ -0,0 +1,26 @@
+console.log("hola mundo");
+
+//variables y constantes//
+var y=1;
+let x=2;
+const z=3;
+
+//declarar variable//
+
+let a;
+
+//inicializar variable//
+//1 declarar e inicializar a la vez//
+let b=1;
+//2 una variable ya declarada se le da un valor//
+a=1;
+
+//Tipos de datos//
+let entero=1;
+let decimal=2.67;
+let texto="hola"; //dato tipo string, o cadena de caracteres//
+let textonum="1";
+
+a=entero+decimal;
+
+console.log(a)
